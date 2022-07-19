621c4bf0da2f90108fdb39018054c7fbdbf7a5ff
Kipngetich33 <khalifngeno@gmail.com>
2022-07-20 00:50:27 +0300

    add changes server IP to serve up changes

--- url_shortner/url_shortner/urls.py
+++ url_shortner/url_shortner/urls.py
@@ -24,8 +24,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
-    path('', RedirectView.as_view(url='main/', permanent=True)),
+    path('', include('main.urls')),
+    # path('', RedirectView.as_view(url='main/', permanent=True)),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
