28202442605cf3f3a011f88e5da8fc6000598ec8
Kipngetich33 <khalifngeno@gmail.com>
2022-07-21 10:16:27 +0300

    clean up the code

--- url_shortner/main/views.py
+++ url_shortner/main/views.py
@@ -99,79 +99,9 @@ def redirect_short_to_long_url(request,shortcode):
         # no match found hence redirect to error page
         return redirect(error_page)
 
-def r(request):    
-    shortcode = Url.objects.get(httpurl = search_name).short_id
-    urls = Url.objects.all()
-    total_clicks = Statistic.get_total_clicks()
-
-    for url in urls:
-        index = calculate_popularity(total_clicks,url.count)
-        url.index = index
-        url.save()
-    return render(request,'makeshort.html',{"message":message,"shortcode":shortcode, "httpurl":httpurl})
-
-def s(request, shortcode):
-    try:
-        is_shortcode = Url.shortcode_exist(shortcode)
-        if is_shortcode == True:
-            requested_url = Url.get_url_by_shorcode(shortcode)
-            requested_url.count +=1
-            requested_url.save()
-            increase_total = Statistic.objects.get(name = 'statistics')
-            increase_total.total_clicks += 1
-            increase_total.save()
-
-            total_clicks1 = Statistic.get_total_clicks()
-            urls = Url.objects.all()
-            for url in urls:
-                index = calculate_popularity(total_clicks1,url.count)
-                url.index = index
-                url.save()
-            return redirect(requested_url.httpurl)
-        else:
-            return redirect(l)
-    except:
-        return redirect(w)
-
-def l(request):
-    message = ''
-    return render(request, 'last.html',{"message":message})
-
-def a(request): 
-    urls = Url.objects.all()
-    total_clicks = Statistic.get_total_clicks()
-
-    for url in urls:
-        index = calculate_popularity(total_clicks,url.count)
-        url.index = index
-        url.save()
-    return render(request,'all.html',{"urls":urls,"total_clicks":total_clicks,"index":index})
-
 def error_page(request):
+    '''
+    View function that displays the error page
+    '''
     return render(request,'wrong.html')
 
-def t(request):
-    '''
-    this is a general test view function that enables me to test the various aspects 
-    of the app before applying changes to an intended view function
-    '''
-    form = UrlForm()
-    if request.method == 'POST':
-
-        form = UrlForm(request.POST)
-
-        if form.is_valid():
-            search_name = form.cleaned_data['entered_url']
-            
-            return render(request,'test2.html',{"message":search_name,"form":form})
-    else:
-        form = UrlForm()
-    return render(request,'test.html',{"form":form})
-
-def p(request):
-    length = 50
-    return render(request,'p.html',{"length": length})
-
-def i(request):
-    length = 50
-    return render(request,'single.html')
