2db80f96571b9dbcfb43faaa4402e13e366a82a1
Kipngetich33 <khalifngeno@gmail.com>
2022-07-21 19:00:33 +0300

    add django settings

--- url_shortner/url_shortner/settings.py
+++ url_shortner/url_shortner/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,10 +84,19 @@ WSGI_APPLICATION = 'url_shortner.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    'default': {     
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',       
+		'NAME': 'shortenerdb',       
+		'USER': 'djangouser',        
+		'PASSWORD': 'Empharse',        
+		'HOST': 'localhost',       
+		'PORT': '',    
+	}
 }
 
 
@@ -124,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
